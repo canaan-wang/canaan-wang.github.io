3434e7c2feffd349cd8e46b2a34e9940acc0aa47
canaan.wang <1300182556@qq.com>
2023-03-31 14:26:54 +0800

    code

---
+++ code/java/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test {
+
+    // 声明一个 ThreadLocal 变量
+    private final ThreadLocal<Object> tl = new ThreadLocal<>();
+
+    public void test(){
+        Thread
+
+    }
+}
